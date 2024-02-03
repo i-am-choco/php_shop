e07c3c5966a009bd7023184435df19f0b2fdc9d0
Lusia.L <lusia.l@videoconf.bo.isac.cnr.it>
2024-02-03 15:31:51 +0800

    feat: user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,17 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\Hash;
 class UserController extends Controller
 {
-    //
-//    public  function register(Request $request)
-//    {
-//        $request->validate([
-//            'name' => 'required|string',
-//            'email' => 'required|email|unique:users',
-//            'password' => 'required|string|min:6',
-//        ]);
-//    }
+    /**
+     * 注册功能
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public  function register(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|string|min:6',
+        ]);
+
+        $user = User::factory()->create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' =>  Hash::make($request->password),
+        ]);
+
+        return response()->json(["user" => $user, 'message' => 'Success' ]);
+    }
 }
