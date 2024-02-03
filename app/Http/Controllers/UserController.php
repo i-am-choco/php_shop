71d24d7159fe96b1df04eb5f5c805b7f3b2866cd
Lusia.L <lusia.l@videoconf.bo.isac.cnr.it>
2024-02-03 14:44:44 +0800

    init

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    //
+//    public  function register(Request $request)
+//    {
+//        $request->validate([
+//            'name' => 'required|string',
+//            'email' => 'required|email|unique:users',
+//            'password' => 'required|string|min:6',
+//        ]);
+//    }
+}
