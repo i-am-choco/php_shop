f02635e522d06c622997dee8623b6531cc6bdbfd
choco <liangpeiqi@hoolinks.com>
2024-02-05 00:37:53 +0800

    feat: 登錄

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 class UserController extends Controller
 {
@@ -14,18 +15,50 @@ class UserController extends Controller
      */
     public  function register(Request $request)
     {
-        $request->validate([
-            'name' => 'required|string',
-            'email' => 'required|email|unique:users',
-            'password' => 'required|string|min:6',
-        ]);
+        try {
+            $request->validate([
+                'name' => 'required|string',
+                'email' => 'required|email|unique:users',
+                'password' => 'required|string|min:6',
+            ]);
 
-        $user = User::factory()->create([
-            'name' => $request->name,
-            'email' => $request->email,
-            'password' =>  Hash::make($request->password),
-        ]);
+            $user = User::factory()->create([
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' =>  Hash::make($request->password),
+            ]);
 
-        return response()->json(["user" => $user, 'message' => 'Success' ]);
+            return response()->json(["user" => $user, 'message' => 'Success']);
+        } catch (\Exception $e) {
+            return response()->json(['message' => $e->getMessage()], 500);
+        }
+    }
+
+    public function login(Request $request)
+    {
+
+        try {
+            $credentials = $request->validate([
+                'email' => 'required|email',
+                'password' => 'required|string',
+            ]);
+
+            if (Auth::attempt($credentials)) {
+                $user = Auth::user();
+                $token = $user->createToken('token')->plainTextToken;
+
+                return response()->json([
+                    'user' => $user,
+                    'access_token' => $token,
+                    'message' => 'Login successful'
+                ]);
+            }
+
+            return response()->json(['message' => 'Invalid credentials'], 401);
+        }catch (\Exception $e)
+        {
+            return  response()->json(['message' => $e->getMessage()], 500);
+        }
+
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -19,3 +19,4 @@ use App\Http\Controllers\UserController;
 //});
 
 Route::post('/register', [UserController::class, "register"]);
+Route::post('/login', [UserController::class, "login"]);
